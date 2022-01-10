a5c102fd94c0bc3e127da260f53a7c6281a1ac82
studiocodigowp <roger.bolivar2011@outlook.com>
2022-01-10 18:06:50 -0500

    javascript1.0

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// SELECTORES INDIVIDUALES
+// getElementById(): selecciona el elemento por su atributo ID
+// querySelector(): Selecciona el elemento con un selector CSS, devolviendo el primer elemento que coincida
+
+//SELECTORES MULTIPLES
+// getElementsByClassName(): Selecciona todos los elementos que tengan la clase especificada
+// getElementsByTagName(): Selecciona todos los elementos por la etiqueta especificada
+// querySelectorAll(): Selecciona todos los elementos con el selector CSS especificado
+
+//-------------------------------------------------------------------------
+
+let boton = document.getElementById("btn-hero");
+
+let segundoParrafo = document.querySelector("#segundo");
+
+let parrafos = document.getElementsByClassName("texto2");
+
+let parrafos2 = document.getElementsByTagName("p");
+
+// let parrafos3 = document.querySelectorAll(".texto1");
+
